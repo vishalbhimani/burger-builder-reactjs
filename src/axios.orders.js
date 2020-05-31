2b501b8c3bcbec43b8e7d8bd374e828be2778552
Vishal Bhimani <vishalbhimani05@gmail.com>
2020-05-31 12:46:39 +0530

    axios.orders deleted

--- src/axios.orders.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-
-const instance = axios.create({
-    baseURL: 'https://react-my-burger-e5a75.firebaseio.com/'
-});
-
-export default instance;
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -5,7 +5,7 @@ import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
-import axios from '../../axios.orders';
+import axios from '../../axios-orders';
 import Spinner from '../../components/UI/Spinner/Spinner';
 import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
 
